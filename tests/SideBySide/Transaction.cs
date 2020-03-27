fc76b23a0639d498cd7fe92eeaae1103519a8396
John Battye <John.Battye@ipfin.co.uk>
2020-03-27 23:01:27 +0000

    testing

--- tests/SideBySide/Transaction.cs
+++ tests/SideBySide/Transaction.cs
@@ -40,6 +40,13 @@ namespace SideBySide
 			m_connection.Execute(@"set global general_log = 0;");
 			var results = connection.Query<string>($"select convert(argument USING utf8) from mysql.general_log where thread_id = {m_connection.ServerThread} order by event_time desc limit 10;");
 			var lastIsolationLevelQuery = results.First(x => x.ToLower().Contains("isolation"));
+
+			if (IsMySqlVersionLessThan57(m_connection.ServerVersion))
+			{
+				Assert.Contains("serializable", lastIsolationLevelQuery.ToLower());
+				return;
+			}
+
 			Assert.Contains(expectedTransactionIsolationLevel.ToLower(), lastIsolationLevelQuery.ToLower());
 		}
 
@@ -65,9 +72,24 @@ namespace SideBySide
 			m_connection.Execute(@"set global general_log = 0;");
 			var results = connection.Query<string>($"select convert(argument USING utf8) from mysql.general_log where thread_id = {m_connection.ServerThread} order by event_time desc limit 10;");
 			var lastStartTransactionQuery = results.First(x => x.ToLower().Contains("start"));
+
+			if (IsMySqlVersionLessThan57(m_connection.ServerVersion))
+			{
+				Assert.Contains("start transaction", lastStartTransactionQuery.ToLower());
+				return;
+			}
+
 			Assert.Contains(expectedTransactionIsolationLevel.ToLower(), lastStartTransactionQuery.ToLower());
 		}
-#endif
+
+		private bool IsMySqlVersionLessThan57(string currentVersionStr)
+		{
+			var version = new Version("5.7");
+			var currentVersion = new Version(currentVersionStr);
+			var result = version.CompareTo(currentVersion);
+			return result > 0;
+		}
+
 
 		readonly TransactionFixture m_database;
 		readonly MySqlConnection m_connection;
