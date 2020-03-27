2f9f5c9ddab0692d5b8996bcaee79c83bba049dd
John Battye <John.Battye@ipfin.co.uk>
2020-03-27 20:19:25 +0000

    testing

--- tests/SideBySide/Transaction.cs
+++ tests/SideBySide/Transaction.cs
@@ -20,11 +20,11 @@ namespace SideBySide
 
 		[Theory]
 		[InlineData(IsolationLevel.ReadUncommitted, "read uncommitted")]
-		[InlineData(IsolationLevel.ReadCommitted, "read committed")]
-		[InlineData(IsolationLevel.RepeatableRead, "repeatable read")]
-		[InlineData(IsolationLevel.Serializable, "serializable")]
-		[InlineData(IsolationLevel.Snapshot, "repeatable read")]
-		[InlineData(IsolationLevel.Unspecified, "repeatable read")]
+		// [InlineData(IsolationLevel.ReadCommitted, "read committed")]
+		// [InlineData(IsolationLevel.RepeatableRead, "repeatable read")]
+		// [InlineData(IsolationLevel.Serializable, "serializable")]
+		// [InlineData(IsolationLevel.Snapshot, "repeatable read")]
+		// [InlineData(IsolationLevel.Unspecified, "repeatable read")]
 		public void DbConnectionIsolationLevel(IsolationLevel inputIsolationLevel, string expectedTransactionIsolationLevel)
 		{
 			DbConnection connection = m_connection;
@@ -38,31 +38,32 @@ namespace SideBySide
 			m_connection.Execute(@"set global general_log = 0;");
 			var results = connection.Query<string>($"select convert(argument USING utf8) from mysql.general_log where thread_id = {m_connection.ServerThread} order by event_time desc limit 10;");
 			var lastIsolationLevelQuery = results.First(x => x.ToLower().Contains("isolation"));
+			Assert.Equal(10, results.Count());
 			Assert.Contains(expectedTransactionIsolationLevel.ToLower(), lastIsolationLevelQuery.ToLower());
 		}
 
-		[Theory]
-		[InlineData(IsolationLevel.ReadUncommitted, "start transaction")]
-		[InlineData(IsolationLevel.ReadCommitted, "start transaction")]
-		[InlineData(IsolationLevel.RepeatableRead, "start transaction")]
-		[InlineData(IsolationLevel.Serializable, "start transaction")]
-		[InlineData(IsolationLevel.Unspecified, "start transaction")]
-		[InlineData(IsolationLevel.Snapshot, "start transaction with consistent snapshot")]
-		public void DbConnectionTransactionCommand(IsolationLevel inputIsolationLevel, string expectedTransactionIsolationLevel)
-		{
-			DbConnection connection = m_connection;
-			m_connection.Execute(@"set global log_output = 'table';");
-			m_connection.Execute(@"set global general_log = 1;");
-			using (var trans = connection.BeginTransaction(inputIsolationLevel))
-			{
-				trans.Commit();
-			}
+		// [Theory]
+		// [InlineData(IsolationLevel.ReadUncommitted, "start transaction")]
+		// [InlineData(IsolationLevel.ReadCommitted, "start transaction")]
+		// [InlineData(IsolationLevel.RepeatableRead, "start transaction")]
+		// [InlineData(IsolationLevel.Serializable, "start transaction")]
+		// [InlineData(IsolationLevel.Unspecified, "start transaction")]
+		// [InlineData(IsolationLevel.Snapshot, "start transaction with consistent snapshot")]
+		// public void DbConnectionTransactionCommand(IsolationLevel inputIsolationLevel, string expectedTransactionIsolationLevel)
+		// {
+		// 	DbConnection connection = m_connection;
+		// 	m_connection.Execute(@"set global log_output = 'table';");
+		// 	m_connection.Execute(@"set global general_log = 1;");
+		// 	using (var trans = connection.BeginTransaction(inputIsolationLevel))
+		// 	{
+		// 		trans.Commit();
+		// 	}
 
-			m_connection.Execute(@"set global general_log = 0;");
-			var results = connection.Query<string>($"select convert(argument USING utf8) from mysql.general_log where thread_id = {m_connection.ServerThread} order by event_time desc limit 10;");
-			var lastStartTransactionQuery = results.First(x => x.ToLower().Contains("start"));
-			Assert.Equal(expectedTransactionIsolationLevel.ToLower(), lastStartTransactionQuery.ToLower());
-		}
+		// 	m_connection.Execute(@"set global general_log = 0;");
+		// 	var results = connection.Query<string>($"select convert(argument USING utf8) from mysql.general_log where thread_id = {m_connection.ServerThread} order by event_time desc limit 10;");
+		// 	var lastStartTransactionQuery = results.First(x => x.ToLower().Contains("start"));
+		// 	Assert.Equal(expectedTransactionIsolationLevel.ToLower(), lastStartTransactionQuery.ToLower());
+		// }
 
 		readonly TransactionFixture m_database;
 		readonly MySqlConnection m_connection;
