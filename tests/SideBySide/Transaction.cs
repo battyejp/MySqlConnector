d5b84a9d2db24ed1be640d62bdcab50ec948e5c4
John Battye <John.Battye@ipfin.co.uk>
2020-03-27 20:31:31 +0000

    testing

--- tests/SideBySide/Transaction.cs
+++ tests/SideBySide/Transaction.cs
@@ -37,8 +37,8 @@ namespace SideBySide
 
 			m_connection.Execute(@"set global general_log = 0;");
 			var results = connection.Query<string>($"select convert(argument USING utf8) from mysql.general_log where thread_id = {m_connection.ServerThread} order by event_time desc limit 10;");
-			var lastIsolationLevelQuery = results.First(x => x.ToLower().Contains("isolation"));
 			Assert.Equal(results.First(), results.Last());
+			var lastIsolationLevelQuery = results.First(x => x.ToLower().Contains("isolation"));
 			Assert.Contains(expectedTransactionIsolationLevel.ToLower(), lastIsolationLevelQuery.ToLower());
 		}
 
